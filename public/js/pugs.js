8bfc8f3f8716979d8a6a0333dd4e2d870619ec0a
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-10-07 15:17:32 -0400

    added all mySQL commands and updated handlebars to show pug in proper list

--- public/js/pugs.js
+++ public/js/pugs.js
@@ -1,29 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const mysql = require("mysql");
 
 const app = express();
+var connection = require("./../../config/connection.js");
 
-// Get list of pugs from db and send to handlebars index page
-app.get("/", function (req, res) {
-    connection.query("SELECT * FROM petPug;", function (err, data) {
-        if (err) { throw err; }
+function showPage(app, __dirname) {
 
-        res.render("index", { pugs: data });
+    // Get list of pugs from db and send to handlebars index page
+    app.get("/", function (req, res) {
+        connection.query("SELECT * FROM petPug;", function (err, data) {
+            if (err) { throw err; }
+            res.render("index", { pugs: data });
+        });
     });
-});
 
 
-// Create a new todo
-app.post("/pugs/new", function (req, res) {
-    // pick a random number from 1-20 to associate with image
-    var imgNum = Math.floor(Math.random() * 20) + 1;
-    var pug = req.body.pug;
-    connection.query('INSERT INTO petPug (pugName,didpet,imgID) VALUES ("' + pug + '","0","' + imgNum + '")', function (err, result) {
-        if (err) { throw err; }
-
-        // Send back the ID of the new todo
-        res.json({ id: result.insertId });
-        console.log({ id: result.insertId });
+    // Add a new pug to the list
+    app.post("/pugs/new", function (req, res) {
+        console.log("pugs new");
+        // pick a random number from 1-20 to associate with image
+        var imgNum = Math.floor(Math.random() * 20) + 1;
+        var pug = req.body.pug;
+        connection.query('INSERT INTO petPug (pugName,didpet,imgID) VALUES ("' + pug + '","0","' + imgNum + '")', function (err, result) {
+            if (err) { throw err; }
+            res.redirect("/");
+        });
     });
-});
+
+    // Walk the pug
+    app.post("/pugs/done", function (req, res) {
+        console.log("pugs done");
+        var pugID = req.body.id;
+        console.log("pugID", pugID);
+        var querySend = 'UPDATE petPug set didpet = "1" where id = "' + pugID + '";';
+        console.log("query",querySend);
+        connection.query(querySend, function (err, data) {
+            console.log("data from pet pug mysql",data);
+            if (err) { throw err; }
+            res.redirect("/");
+        });
+    });
+
+}
+module.exports = showPage;
--- server.js
+++ server.js
@@ -7,19 +7,6 @@ const mysql = require("mysql");
 const app = express();
 const PORT = process.env.PORT || 4100;
 
-var connection;
-
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "pugs_db"
-    });
-};
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -27,7 +14,9 @@ app.set("view engine", "handlebars");
 
 // grab the file that has the routes and logic
 var routes = require("./public/js/pugs.js");
+routes(app, __dirname);
 
-// app.use("/", routes); not sure if we need this??
 
-app.listen(PORT);
+app.listen(PORT, function() {
+    console.log("app listening on PORT " + PORT);
+});
